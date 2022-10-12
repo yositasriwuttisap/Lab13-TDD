34504ebaa5af92b3dc077385b80505ad5a53bbe0
“Jitchaya” <Jitchaya.v@kkumail.com>
2022-10-12 15:54:17 +0700

    Check step 4: No Fizz No Buzz No FizzBuzz Complete

--- src/main/Multinumber.java
+++ src/main/Multinumber.java
@@ -1,16 +1,22 @@
 package main;
 
 public class Multinumber {
-		public String check(int num){
-			if(num %3 == 0 && num %5 == 0) {
-				return "FizzBuzz";
-			}
-			else if(num %3 == 0){
-		  		return "Fizz";
-		    }
-		  	else if(num %5 == 0) {
-		  		return "Buzz";
-		  	}
-		    return "0";
-			}
+	public String check(int num){
+		if(num>0 && num<101) {
+			
+		if(num %3 == 0 && num %5 == 0) {
+			return "FizzBuzz";
+		}
+		else if(num %3 == 0){
+	  		return "Fizz";
+	    }
+	  	else if(num %5 == 0) {
+	  		return "Buzz";
+	  	}
+	  	else {
+	  		String j = String.valueOf(num);
+	  		return j;
+	  	}}
+		return "over 1-100";
+		}
 	}
