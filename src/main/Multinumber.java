d17ae8c4f2f7a64c045c4d16a6a1608b7b9622bd
“Jitchaya” <Jitchaya.v@kkumail.com>
2022-10-12 15:08:58 +0700

    Check step 3: FizzBuzz Complete

--- src/main/Multinumber.java
+++ src/main/Multinumber.java
@@ -2,10 +2,13 @@ package main;
 
 public class Multinumber {
 		public String check(int num){
-		  	if(num %3 == 0){
+			if(num %3 == 0 && num %5 == 0) {
+				return "FizzBuzz";
+			}
+			else if(num %3 == 0){
 		  		return "Fizz";
 		    }
-		  	else if(num%5 == 0) {
+		  	else if(num %5 == 0) {
 		  		return "Buzz";
 		  	}
 		    return "0";
