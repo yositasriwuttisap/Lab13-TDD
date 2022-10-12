4bbcee61020e6b9703d7c2c71c388ec45a438e7c
yositasriwuttisap <yosita.sr@kkumail.com>
2022-10-12 15:50:59 +0700

    step 4: TC04 - Number = Integer 1-100 No Fizz, No Buzz, No FizzBuzz

--- src/test/TestNumber.java
+++ src/test/TestNumber.java
@@ -42,4 +42,18 @@ class TestNumber {
 		String result = multi.check(input);
 		assertEquals("FizzBuzz", result);
 		}
+	
+	@DisplayName("TC04 - Number = Integer 1-100 No Fizz, No Buzz, No FizzBuzz")
+	@ParameterizedTest
+	@ValueSource(strings = {"1,1","2,2","4,4","7,7","8,8","11,11","13,13","14,14","16,16","17,17","19,19","22,22","23,23","26,26","28,28"
+			,"29,29","31,31","32,32","34,34","37,37","38,38","41,41","43,43","44,44","46,46","47,47","49,49","52,52","53,53"
+			,"56,56","58,58","59,59","61,61","62,62","64,64","67,67","68,68","71,71","73,73","74,74","76,76","77,77","79,79"
+			,"82,82","83,83","86,86","88,88","89,89","91,91","92,92","94,94","97,97","98,98"})
+	void testTC04(String input) {
+		String[] n = input.split(",");
+		int number1 = Integer.parseInt(n[0]);
+		String expect = n[1];
+		String result = multi.check(number1);
+		assertEquals(expect, result);
+		}
 }
