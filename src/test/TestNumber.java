42ea0473d7f009a40c15ad46a0957bab20459c6c
yositasriwuttisap <yosita.sr@kkumail.com>
2022-10-12 14:36:18 +0700

    step 1: TC01 - Number = Integer 1-100 Fizz

--- src/test/TestNumber.java
+++ src/test/TestNumber.java
@@ -19,12 +19,11 @@ class TestNumber {
 	}
 	
 	
-	@DisplayName("TC01 - Number = Integer 1-100 No FizzBuzz, Fizz, Buzz")
+	@DisplayName("TC01 - Number = Integer 1-100 Fizz")
 	@ParameterizedTest
-	@ValueSource(ints = {1,2,4,7,8,11,13,14,16,17,19,22,23,26,28,29,31,32,34,37,38,41,43,44,46,47,49,
-						52,53,56,58,59,61,62,64,67,68,71,73,74,76,77,79,82,83,86,88,89,91,92,94,97,98})
+	@ValueSource(ints = {3,6,9,12,18,21,24,27,33,36,39,42,48,51,54,57,63,66,69,72,78,81,84,87,93,96,99})
 	void testTC01(int input) {
 		String result = multi.check(input);
-		assertEquals(input, result);
+		assertEquals("Fizz", result);
 		}
 }
